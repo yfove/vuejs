36f55a0ade25360df8cef9bb138b6060cb6e1024
curtis chat <curtis.ch.chat@gmail.com>
2021-04-09 23:23:38 -0600

    handling user input

--- index.js
+++ index.js
@@ -32,4 +32,23 @@ var app4 = new Vue({
   }
 })
 
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
+
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
+
 app.message = 'I have changed the data'
