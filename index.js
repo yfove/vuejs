e80abd16bf5413f9691a76b1c55f030148511961
curtis chat <curtis.ch.chat@gmail.com>
2021-04-09 22:58:31 -0600

    conditionals and loops

--- index.js
+++ index.js
@@ -14,4 +14,22 @@ var app2 = new Vue({
   }
 })
 
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen: true
+  }
+})
+
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      {text: 'Learn JavaScript'},
+      {text: 'Learn Vue'},
+      {text: 'Build something awesome'}
+    ]
+  }
+})
+
 app.message = 'I have changed the data'
