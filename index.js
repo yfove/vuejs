84105ca877071dd35e4340bf440a4f8c4b030373
curtis chat <curtis.ch.chat@gmail.com>
2021-04-09 22:36:03 -0600

    bind attributes

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+// text interpolation
 var app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue!'
+  }
+})
+
+// bind element attributes
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'You loaded this page on ' + new Date().toLocaleString()
   }
 })
 
